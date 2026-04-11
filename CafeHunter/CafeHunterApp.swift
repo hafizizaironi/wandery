42f7f16ec91328a930d9eeb697bdeb60b91c841b
hafizizaironi <hafizizaironi@gmail.com>
2026-04-12 03:47:20 +0800

    Initial Commit

---
+++ CafeHunter/CafeHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeHunterApp.swift
+//  CafeHunter
+//
+//  Created by Hafizi Zaironi on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CafeHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
